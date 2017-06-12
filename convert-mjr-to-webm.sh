076291a8ede085bafea8e295d52c31093bc560a0
Giordano Cardillo <giordano.cardillo@gmail.com>
2017-06-12 11:07:52 +0000

    Fixed convert script

--- convert-mjr-to-webm.sh
+++ convert-mjr-to-webm.sh
@@ -2,7 +2,7 @@
 set -e
 
 FILES=./*video.mjr
-i=1
+FAILED=""
 
 if [ ! -x "$(command -v ffmpeg)" ]; then
   printf "\033[31mERROR\033[0m This script requires \033[1mffmpeg\033[0m\n"
@@ -23,33 +23,32 @@ for video in $FILES
 do
   filename=$(echo $video | awk -F"-" '{print $1"-"$2"-"$3"-"$4"-"$5}')
   if [ -f $filename"-audio.mjr" ]; then
-    printf "\033[36mProcessing\033[0m $filename"
-    printf "  -> \033[35mExtracting video\033[0m"
+    printf "\033[36mProcessing\033[0m $filename\n"
+    printf "  -> \033[35mExtracting video\033[0m\n"
     janus-pp-rec $video /tmp/janus-recordings/video.webm >/dev/null 2>&1
     RESULT=$?
-    printf "  -> \033[35mExtracting audio\033[0m"
+    printf "  -> \033[35mExtracting audio\033[0m\n"
     janus-pp-rec $filename"-audio.mjr" /tmp/janus-recordings/audio.opus >/dev/null 2>&1
     RESULT=$?
-    printf "  -> \033[33mMerging\033[0m"
+    printf "  -> \033[33mMerging\033[0m\n"
     ffmpeg -i /tmp/janus-recordings/audio.opus -i /tmp/janus-recordings/video.webm -y -c:v copy -c:a copy $filename.webm  >/dev/null 2>&1
     RESULT=$?
     if [ $RESULT -eq 0 ]; then
       rm -rf /tmp/janus-recordings/video.webm
       rm -rf /tmp/janus-recordings/audio.opus
       rm -rf "$filename"*.mjr
-      printf "  -> \033[32mComplete\033[0m"
+      printf "  -> \033[32mComplete\033[0m\n"
     else
       rm -rf "$filename"*.webm
-      FAILED[$i]=$filename
-      $i++
-      printf "  -> \033[31mFailed\033[0m"
+      FAILED="$FAILED "$filename
+      printf "  -> \033[31mFailed\033[0m\n"
     fi
-    echo
   fi
 done
 printf "\033[36mFinished processing\033[0m\n"
-if [[ ${FAILED[@]} ]]; then
-  echo -e "\033[31mFailed converting following recordings\033[0m"
-  printf '  %s\n' "${FAILED[@]}"
-  echo
+if [ -n "$FAILED" ]; then
+  printf "\n\033[31mFailed converting following recordings\033[0m\n"
+  for file in $FAILED; do
+    printf "  $file\n"
+  done
 fi
