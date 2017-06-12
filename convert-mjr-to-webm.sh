c426403acf4fbdf2c60ca9afaa91299a4e0576bf
Giordano Cardillo <giordano.cardillo@gmail.com>
2017-06-12 10:22:03 +0000

    Updated convert mjr2webm

--- convert-mjr-to-webm.sh
+++ convert-mjr-to-webm.sh
@@ -2,17 +2,15 @@
 set -e
 
 FILES=./*video.mjr
-FAILED=()
+i=1
 
 if [ ! -x "$(command -v ffmpeg)" ]; then
-  echo -e "\033[31mERROR\033[0m This script requires \033[1mffmpeg\033[0m"
-  echo
+  printf "\033[31mERROR\033[0m This script requires \033[1mffmpeg\033[0m\n"
   exit 1
 fi
 
 if [ ! -x "$(command -v janus-pp-rec)" ]; then
-  echo -e "\033[31mERROR\033[0m This script requires \033[1mjanus-pp-rec\033[0m"
-  echo
+  printf "\033[31mERROR\033[0m This script requires \033[1mjanus-pp-rec\033[0m\n"
   exit 1
 fi
 
@@ -25,31 +23,31 @@ for video in $FILES
 do
   filename=$(echo $video | awk -F"-" '{print $1"-"$2"-"$3"-"$4"-"$5}')
   if [ -f $filename"-audio.mjr" ]; then
-    echo -e "\033[36mProcessing\033[0m $filename"
-    echo -e "  -> \033[35mExtracting video\033[0m"
+    printf "\033[36mProcessing\033[0m $filename"
+    printf "  -> \033[35mExtracting video\033[0m"
     janus-pp-rec $video /tmp/janus-recordings/video.webm >/dev/null 2>&1
     RESULT=$?
-    echo -e "  -> \033[35mExtracting audio\033[0m"
+    printf "  -> \033[35mExtracting audio\033[0m"
     janus-pp-rec $filename"-audio.mjr" /tmp/janus-recordings/audio.opus >/dev/null 2>&1
     RESULT=$?
-    echo -e "  -> \033[33mMerging\033[0m"
+    printf "  -> \033[33mMerging\033[0m"
     ffmpeg -i /tmp/janus-recordings/audio.opus -i /tmp/janus-recordings/video.webm -y -c:v copy -c:a copy $filename.webm  >/dev/null 2>&1
     RESULT=$?
     if [ $RESULT -eq 0 ]; then
       rm -rf /tmp/janus-recordings/video.webm
       rm -rf /tmp/janus-recordings/audio.opus
       rm -rf "$filename"*.mjr
-      echo -e "  -> \033[32mComplete\033[0m"
+      printf "  -> \033[32mComplete\033[0m"
     else
       rm -rf "$filename"*.webm
-      FAILED+=($filename)
-      echo -e "  -> \033[31mFailed\033[0m"
+      FAILED[$i]=$filename
+      $i++
+      printf "  -> \033[31mFailed\033[0m"
     fi
     echo
   fi
 done
-echo -e "\033[36mFinished processing\033[0m"
-echo
+printf "\033[36mFinished processing\033[0m\n"
 if [[ ${FAILED[@]} ]]; then
   echo -e "\033[31mFailed converting following recordings\033[0m"
   printf '  %s\n' "${FAILED[@]}"
